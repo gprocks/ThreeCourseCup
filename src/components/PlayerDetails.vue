9af4d6fddd0c68d4726e598b04b1a7f76e23f706
Gerard <gp_rocks@hotmail.com>
2020-09-06 19:44:37 +0100

    Add icons

--- src/components/PlayerDetails.vue
+++ src/components/PlayerDetails.vue
@@ -4,6 +4,10 @@
     <div class="player" v-for="(item,index) in rankedList" :key="index">
       <div>
         <span>Player:</span>
+        <span v-if="index===0">&#128081;</span>
+        <span v-if="index===1">&#129352;</span>
+        <span v-if="index===2">&#129353;</span>
+        <span v-if="index===3">&#129364;</span>
         {{item.player}}
       </div>
       <div>
